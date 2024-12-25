b682fab5d5d89ba91056c4c3381f29ed68bea9a2
shubhamiscodding <shubham.modi.cg@gmail.com>
2024-12-25 21:45:21 +0530

    almost done in second page

--- progcap-vite/src/parts/SecondPage.jsx
+++ progcap-vite/src/parts/SecondPage.jsx
@@ -2,7 +2,76 @@ let SecondPage = function (){
     return(
         <>
         <div className="page2">
-            
+            <div className="info4">
+                <div className="img4">
+                    <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a4d04028946a182aebefea_Lines-long.svg" alt="" />
+                    <img src="../../img2.png" alt="" className="img4-2"/>
+                </div>
+                <div className="info4-1">
+                    <h2>One App for all your Payments & Financial Needs</h2>
+                    <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/6197581993f2df6230b7bfb1_Play%20Store%20Button.png" alt="" />
+                    <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/6197581975b7d9598f338659_App%20Store%20Button.png" alt="" />
+                </div>
+            </div>
+            <div className="scroll1">
+                <h1>Accolades</h1>
+                <div className="conatiner">
+                    <div className="contain">
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/638ef498eb1a4d2888d42dab_India_Fintech_Forum.png" alt="" className="imglogo"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/638ef498eb1a4d2888d42dab_India_Fintech_Forum.png" alt="" className="imglogo"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/633acad93e2ee8ced743f931_Entreprenur%20awards%202022.PNG" alt="" className="imglogosmall"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/62550c96b454a50b4dbb67dd_bw%20business.png" alt="" className="imglogosmaller"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/62550c96b454a50b4dbb67dd_bw%20business.png" alt="" className="imglogosmaller"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61977f460873830035cd0d9a_Gold%20winner.png" alt="" className="imglogo"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/619782a2ab885f5c268846ad_National%20Awards.png" alt="" className="imglogo"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                        <div className="contain-1">
+                            <img  src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61977f4639f978709d8a03e0_Tracon.png" alt="" className="imglogo"/>
+                            <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/61a629f7bb09a726faa4b552_v-shape-acc.svg" alt="" className="imgbg" />
+                            <p className="tittle">Women Leader in Fintech</p>
+                            <p className="text">Pallavi Shrivastava was awarded as the Women Leader in Fintech at IFTA 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="review">
+                <h1>Hear What Our Partners
+                Have to Say</h1>
+            </div>
         </div>
         </>
     )
